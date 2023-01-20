b78b1c6047e691b92d9bdbf2cceeb3fb8ee87f76
faizanarshad0 <123149939+faizanarshad0@users.noreply.github.com>
2023-01-20 20:30:18 +0500

    Add files via upload

---
+++ Array ADT.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+class Array{
+  
+        private:
+        	int arr[10];
+        	int length;
+        	int size;
+        
+		public:
+			Array(){
+				length = 5;
+				size = 10;
+			}
+			
+			int get_length(){
+				return length;
+			}
+			
+			void append(int arr[], int key){
+				arr[length] = key;
+				length++;
+			}
+			
+			void insert(int arr[], int key, int index){
+			
+				
+					for(int i = length; i>index; i--)
+					    arr[i] = arr[i-1];
+					arr[index] = key;
+					length++;
+		
+			   
+		    }
+		    
+		   /* void inddelete(int arr[],int length,int index)
+		    {
+		    	for(int i=index;int<length-1;i++)
+		    	{
+		    		arr[i] = arr[i + 1];
+				}
+			}
+		    */
+		    
+		    void Delete(int arr[]) {
+
+		int num;
+		
+		cout << "please Enter the Number:";
+		cin >> num;
+		for (int i = 0; i < length; i++)
+		{
+			if (arr[i] == num)
+			{
+				for (int j = i; j<length-1; j++)
+					arr[j] = arr[j+1];
+				i--;
+				
+				
+				length--;
+			}
+			
+		}
+	}
+		void display(int ar[]){
+			cout<<"The values are: ";
+			for(int i = 0; i<length; i++)
+				cout<<ar[i]<<endl;
+		}
+};
+
+
+
+int main()
+{
+	
+	int a[10];
+	Array arr; 
+	int length = arr.get_length();
+	cout<<"Enter the values: ";
+	for(int i = 0; i<length; i++)
+	   cin>>a[i];
+	arr.display(a);
+	arr.append(a, 34);
+	arr.display(a);
+	arr.insert(a,50,3);
+	arr.display(a);
+	  // inddelete(arr,length,index)
+	   //size-=1;
+	   arr.Delete(a);
+	   arr.display(a);
+}
